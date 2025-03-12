ed68f617e48cd4dd480a0999aabe00908462a753
ADolmos194 <AyltonDolmos194@gmail.com>
2025-03-12 05:54:02 -0500

    nueva database

--- backend/settings.py
+++ backend/settings.py
@@ -19,10 +19,22 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
-    'http://127.0.0.1:4200',
+    'https://proyectometeoritoswtesis.netlify.app'
 )
 
-# Application definition
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+]
+
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+    'OPTIONS',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -31,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'app',
     'app_registros'
 ]
@@ -39,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,9 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # La carpeta donde están tus archivos estáticos adicionales
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # La carpeta donde se recopilarán los archivos estáticos para producción
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
