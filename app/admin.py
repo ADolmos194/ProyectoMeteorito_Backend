0f4bf69b953c16724c4a87966e4d19258052435b
ADolmos194 <AyltonDolmos194@gmail.com>
2025-03-14 20:56:28 -0500

    nuevo backend

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from app.models import *
 
-admin.site.register([Estado, Tipodocumento])
+admin.site.register([Estado, Tipodocumento, Estadopagos, Cuotas, Cuotaspagadas])
